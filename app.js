b21f373206087affe8d54fa36a87ff27524eea46
Sarath Mantrala <sarath.mantrala@gmail.com>
2016-04-01 16:08:53 -0700

    trying github pages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = angular.module("angularPizza", ['ngRoute']);
 
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
   $routeProvider
     .when('/pizza',
         {
