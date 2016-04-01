37ad558025dddb175dad4fbb6f56109f9e45bdbc
Sarath Mantrala <sarath.mantrala@gmail.com>
2016-04-01 16:47:18 -0700

    more gh-pages fun

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = angular.module("angularPizza", ['ngRoute']);
 
 app.config(function($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
+  // $locationProvider.html5Mode(true);
   $routeProvider
     .when('/pizza',
         {
